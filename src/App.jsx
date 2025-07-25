297f100b62c340ddcf393deb20900c4714364a23
gurvirc <gca83@sfu.ca>
2025-07-24 22:41:35 -0700

    feat: create hangman UI with game status, word display, keyboard, and language chips

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { languages } from "./Languages"
+import React from "react"
+export default function App(){
+  const [word, setWord]= React.useState("react")
+  const [guessedLetters, setGuessedLetters]=React.useState([])
+
+  const keyboard="abcdefghijklmnopqrstuvwxyz"
+
+  const keyboardElements= keyboard.split("").map(letter=>(
+    <button className="keyboard-button">{letter.toUpperCase()}</button>
+
+  ))
+  
+  const wordElement= word.split("").map(letter=>(
+    <h1 className="letters">{guessedLetters.includes(letter)?letter.toUpperCase():""}</h1>
+  ))
+
+  const langaugeElements= languages.map(language=>{
+    const styles={
+        backgroundColor: language.backgroundColor,
+        color: language.color
+    }
+    return (
+    <span style={styles} className="languages-chips">{language.name}</span>
+    )
+  })
+
+
+
+
+
+
+
+
+
+
+  return(
+    <main>
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+      </header>
+      <section className="game-status">
+        <h1>You win!</h1>
+        <h2>Well done! 🎉</h2>
+      </section>
+      <section className="language-elements">{langaugeElements}</section>
+      <section className="word-area">{wordElement}</section>
+      <section className="keyboard">{keyboardElements}</section>
+      
+
+    </main>
+  )
+}
