3a3a2326697ad36019eb6a72fe8e1b2719c2acc9
gurvirc <gca83@sfu.ca>
2025-07-28 13:36:35 -0700

    added confetti when the game is won

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { languages } from "./Languages"
 import React from "react"
 import { getFarewellText, getRandomWord } from "./Utils"
+import Confetti from "react-confetti"
 
 
 
@@ -69,6 +70,7 @@ export default function App(){
 
   return(
      <main>
+      {isGameWon && <Confetti />}
       <header>
         <h1>Assembly: Endgame</h1>
         <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
